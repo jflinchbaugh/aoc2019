2335b6a72ed4cd35c8fd4c6d51b8ce036b27d0fe
John Flinchbaugh <john@hjsoft.com>
2019-12-05 03:52:23 -0500

    tests for existing behavior of run-int-code

--- test/aoc2019/core_test.clj
+++ test/aoc2019/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [aoc2019.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-run-int-code-examples
+  (testing "examples from aoc"
+    (are [start-state _ end-state] (= end-state (run-int-code start-state))
+      [] :causes :error-no-op
+      [3] :causes :error-bad-op
+      [99] :becomes [99]
+      [1,0,0,0,99] :becomes [2,0,0,0,99]
+      [2,3,0,3,99] :becomes [2,3,0,6,99]
+      [2,4,4,5,99,0] :becomes [2,4,4,5,99,9801]
+      [1,1,1,4,99,5,6,0,99] :becomes [30,1,1,4,2,5,6,0,99])))
