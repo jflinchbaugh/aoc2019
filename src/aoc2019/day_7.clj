4ca810f5654a5bab8bc2650fceac9545d4ae2635
John Flinchbaugh <john@hjsoft.com>
2019-12-31 03:36:10 -0500

    day 7: param the initial input for feedback

--- src/aoc2019/day_7.clj
+++ src/aoc2019/day_7.clj
@@ -17,9 +17,13 @@
 
 (defn run-series
   "run the series of computers in the given phases"
-  [[pos0 pos1 pos2 pos3 pos4] [pgm0 pgm1 pgm2 pgm3 pgm4] [ph0 ph1 ph2 ph3 ph4]]
+  [
+   input
+   [pos0 pos1 pos2 pos3 pos4]
+   [pgm0 pgm1 pgm2 pgm3 pgm4]
+   [ph0 ph1 ph2 ph3 ph4]]
   (let [
-        run-0 (run-once pos0 pgm0 ph0 0)
+        run-0 (run-once pos0 pgm0 ph0 input)
         run-1 (run-once pos1 pgm1 ph1 (-> run-0 :outputs first))
         run-2 (run-once pos2 pgm2 ph2 (-> run-1 :outputs first))
         run-3 (run-once pos3 pgm3 ph3 (-> run-2 :outputs first))
@@ -28,12 +32,12 @@
     [run-0 run-1 run-2 run-3 run-4]))
 
 (defn part-1 [pgm]
-  (apply max (map (comp first #(% :outputs) (partial run-series (repeat 0) (repeat pgm))) all-phases)))
+  (apply max (map (comp first :outputs last (partial run-series 0 (repeat 0) (repeat pgm))) all-phases)))
 
 (comment
 
-  (run-series (repeat 0) (repeat program) [0 1 2 3 4])
+  (run-series 0 (repeat 0) (repeat program) [0 1 2 3 4])
 
   (part-1 program)
-;; => 117312
+
   )
