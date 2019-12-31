a4693c0eabcec9f149cc24905c08dc0b9fbc9765
John Flinchbaugh <john@hjsoft.com>
2019-12-31 00:53:50 -0500

    refactor day 7 to allow feedback for part 2

--- src/aoc2019/day_7.clj
+++ src/aoc2019/day_7.clj
@@ -17,31 +17,23 @@
 
 (defn run-series
   "run the series of computers in the given phases"
-  [pgm [p0 p1 p2 p3 p4]]
-  (->>
-    0
-    (run-once 0 pgm p0)
-    :outputs
-    first
-    (run-once 0 pgm p1)
-    :outputs
-    first
-    (run-once 0 pgm p2)
-    :outputs
-    first
-    (run-once 0 pgm p3)
-    :outputs
-    first
-    (run-once 0 pgm p4)
-    :outputs
-    first
-    ))
+  [[pos0 pos1 pos2 pos3 pos4] [pgm0 pgm1 pgm2 pgm3 pgm4] [ph0 ph1 ph2 ph3 ph4]]
+  (let [
+        run-0 (run-once pos0 pgm0 ph0 0)
+        run-1 (run-once pos1 pgm1 ph1 (-> run-0 :outputs first))
+        run-2 (run-once pos2 pgm2 ph2 (-> run-1 :outputs first))
+        run-3 (run-once pos3 pgm3 ph3 (-> run-2 :outputs first))
+        run-4 (run-once pos4 pgm4 ph4 (-> run-3 :outputs first))
+        ]
+    [run-0 run-1 run-2 run-3 run-4]))
 
 (defn part-1 [pgm]
-  (apply max (map (partial run-series pgm) all-phases)))
+  (apply max (map (comp first #(% :outputs) (partial run-series (repeat 0) (repeat pgm))) all-phases)))
 
 (comment
 
-  (part-1 program)
+  (run-series (repeat 0) (repeat program) [0 1 2 3 4])
 
+  (part-1 program)
+;; => 117312
   )
