0dabaa1d73a683c089336b3a5afc9da28d0c25f6
John Flinchbaugh <john@hjsoft.com>
2020-01-07 11:38:06 -0500

    day 7 part 2
    * refactor run-series to not always allow phases to be optional
    * tests from examples for run-feedback
    * part-2

--- src/aoc2019/day_7.clj
+++ src/aoc2019/day_7.clj
@@ -12,8 +12,8 @@
 
 (defn run-once
   "run program once through the int-code computer at a given phase and input"
-  ([pos pgm phase input]
-   (run-int-code pos pgm [phase input] [])))
+  ([pos pgm input]
+   (run-int-code pos pgm input [])))
 
 (defn run-series
   "run the series of computers in the given phases"
@@ -23,21 +23,43 @@
    [pgm0 pgm1 pgm2 pgm3 pgm4]
    [ph0 ph1 ph2 ph3 ph4]]
   (let [
-        run-0 (run-once pos0 pgm0 ph0 input)
-        run-1 (run-once pos1 pgm1 ph1 (-> run-0 :outputs first))
-        run-2 (run-once pos2 pgm2 ph2 (-> run-1 :outputs first))
-        run-3 (run-once pos3 pgm3 ph3 (-> run-2 :outputs first))
-        run-4 (run-once pos4 pgm4 ph4 (-> run-3 :outputs first))
+        run-0 (run-once pos0 pgm0 (remove nil? [ph0 input]))
+        run-1 (run-once pos1 pgm1 (remove nil? [ph1 (-> run-0 :outputs first)]))
+        run-2 (run-once pos2 pgm2 (remove nil? [ph2 (-> run-1 :outputs first)]))
+        run-3 (run-once pos3 pgm3 (remove nil? [ph3 (-> run-2 :outputs first)]))
+        run-4 (run-once pos4 pgm4 (remove nil? [ph4 (-> run-3 :outputs first)]))
         ]
     [run-0 run-1 run-2 run-3 run-4]))
+
+(defn run-feedback [input positions pgms phases]
+  (let [series (run-series input positions pgms phases)
+        output (-> series last :outputs last)
+        continues (map :continue series)
+        next-positions (map first continues)
+        next-states (map second continues)
+        ]
+    (if (nil? (last continues))
+      series
+      (recur output next-positions next-states []))))
 
 (defn part-1 [pgm]
   (apply max (map (comp first :outputs last (partial run-series 0 (repeat 0) (repeat pgm))) all-phases)))
 
+(defn part-2 [pgm]
+  (-> (map
+     (comp
+       last
+       :outputs
+       last
+       (partial run-feedback 0 (repeat 0) (repeat pgm)))
+     all-loopback-phases)
+    sort
+    last))
+
 (comment
 
-  (run-series 0 (repeat 0) (repeat program) [0 1 2 3 4])
-
   (part-1 program)
+
+  (part-2 program)
 
   )
